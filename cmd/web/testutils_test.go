a351ee2071562215171d6bfad6f5039045e372bd
Joseph Ezekiel <theolujay@gmail.com>
2026-05-05 14:22:27 +0100

    test: refactor ping handler test and add test server utility

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	// Initialize the test server
+	ts := httptest.NewTLSServer(h)
+
+	// Initialize a new cookie jar
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the cookie jar to the test server client. Any response
+	// cookies will now be stored and sent with subsequent requests
+	// when using this client
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the test server client by
+	// setting a custom CheckRedirect function. This function will
+	// be called whenever a 3xx response is received by the client,
+	// and by always returning a http.ErrUseLastResponse error it
+	// forces the client to immediately return the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &testServer{ts}
+}
+
+// get() makes a GET request to a given url path using the test server client, and
+// returns the response satus code, headers and body
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
