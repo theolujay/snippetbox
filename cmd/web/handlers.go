8fbdf6f510c1ef6381d18e4e5af4ef12e7b9bfa0
Joseph Ezekiel <theolujay@gmail.com>
2026-05-06 08:27:11 +0100

    feat: add about page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -27,6 +27,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "about.tmpl", data)
+}
+
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	// httprouter stores values of named parameters in the request context when
 	// parsing the request
